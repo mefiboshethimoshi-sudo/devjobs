3789b2154e40e7fbe0cec0cfed1d0d45f077b3f5
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-05-12 15:53:27 +0300

    updated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import Header from "./components/Header";
-
 function App() {
   return (
-    <div className="app">
-      <Header />
-
-      <main className="main-content">
-        <h2>Welcome to DevJobs</h2>
-
-        <p>
-          Your remote developer career starts here.
-        </p>
-      </main>
+    <div>
+      <h1>DevJobs: System Online ⚡</h1>
+      <p>Mission Pilot: Aisha Mwamba</p>
     </div>
   );
 }
