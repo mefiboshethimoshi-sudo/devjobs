690225fe42da35ecc1dab03f0a148bfc4a2893c3
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-05-20 20:06:34 +0300

    Add Badge component export to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as JobCard } from "./JobCard";
 export { default as JobList } from "./JobList";
+export { default as Badge } from "./Badge";
