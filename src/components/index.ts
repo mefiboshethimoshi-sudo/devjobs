06992e53ee50605f1b079acd196932596846ad36
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-05-20 19:38:31 +0300

    Add Header and JobList exports to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as JobCard } from './JobCard'
+export { default as Header } from "./Header";
+export { default as JobCard } from "./JobCard";
+export { default as JobList } from "./JobList";
