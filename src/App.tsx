5ea4ba27177481b77951c6d13e1a4d41eed947a4
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-05-21 23:44:37 +0300

    Add return type to App function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import JobList from "./components/JobList";
 import { JOBS } from "./data/jobs";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <JobList jobs={JOBS} />
