f03032bffdbd1a2ec2e3ae31d77ec20483ebe3b3
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-05-22 00:02:33 +0300

    Refactor Job interface to use Company type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
+export interface Company {
+  name: string;
+  logo?: string;
+  website?: string;
+}
+
 export interface Job {
   id: number;
   title: string;
-  company: string;
+  company: Company;
   location: string;
   salary?: string;
   isRemote: boolean;
