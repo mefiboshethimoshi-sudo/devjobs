9d5ff3e522f8e20d6f8ef8c601c4e785a694da48
mefiboshethimoshi-sudo <mefiboshethimoshi@gmail.com>
2026-06-02 14:43:35 +0300

    Enhance JobList to support saving jobs
    Updated JobList component to accept savedIds and onSave props for job saving functionality.

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,23 +1,15 @@
-import JobCard from "./JobCard";
-import { Job } from "../types";
+import JobCard from './JobCard';
 
-interface JobListProps {
-  jobs: Job[];
-}
-
-function JobList({ jobs }: JobListProps): JSX.Element {
-  if (jobs.length === 0) {
-    return (
-      <div className="empty-state">
-        <p>No jobs found. Try adjusting your search.</p>
-      </div>
-    );
-  }
-
+function JobList({ jobs, savedIds, onSave }: any) {
   return (
-    <div className="job-list">
-      {jobs.map((job) => (
-        <JobCard key={job.id} {...job} />
+    <div>
+      {jobs.map((job: any) => (
+        <JobCard
+          key={job.id}
+          {...job}
+          isSaved={savedIds.has(job.id)}
+          onSave={onSave}
+        />
       ))}
     </div>
   );
